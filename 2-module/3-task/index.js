8274d291f5f012140294c872909ba2afba3dbfa6
Leonid <sapiroleonid34@gmail.com>
2024-10-01 09:27:35 +0300

    modified:   2-module/3-task/index.js

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,17 @@
 let calculator = {
   // ваш код
+  read() {
+    calculator.a = +prompt('vvedi a', '0');
+    calculator.b = +prompt('vvedi b', '0');
+},
+
+sum() {
+    return (this.a + this.b);
+    },
+
+mul() {
+    return (this.a * this.b)
+    },
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
