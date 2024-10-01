49115122a4da0e33ddb0a071b275a7fd8463ddff
Leonid <sapiroleonid34@gmail.com>
2024-10-02 00:15:26 +0300

    modified:   3-module/3-task/index.js

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,33 @@
-function camelize(str) {
-  // ваш код...
-}
+// function camelize(str) {
+//   // ваш код...
+
+
+
+  // let str = prompt('vvedi str snake-case','abc-def');
+
+  function camelize(str) {
+  
+          let words = str.split('-');
+    
+    //       for (let word of words ) {
+    //            return word[0].toUpperCase() + word.slice(1);
+    //           };
+
+    //       let result = words.join('');
+    
+    // let result = words.map( (item) = {item[0].toUpperCase() + item.slice(1) } 
+
+    let zagl = words.map( function letter (word) {
+      return word[0].toUpperCase() + word.slice(1) 
+      } );
+
+let result = zagl.join('')
+
+    console.log(result);
+
+  };
+  
+  // console.log(camelize(str));
+  // camelize(str);
+
+// }
