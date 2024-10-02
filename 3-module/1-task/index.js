aae7070d9dd4d2151b00d4639b99a2420cfd67af
Leonid <sapiroleonid34@gmail.com>
2024-10-02 10:19:36 +0300

    3-module/1-task solve

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,25 @@
 function namify(users) {
   // ваш код...
+  let usersNames = users.map((user) => {
+    return user.name 
+});
 }
+
+
+
+// let vasya = { name: 'Вася', age: 25 };
+// let petya = { name: 'Петя', age: 30 };
+// let masha = { name: 'Маша', age: 28 };
+
+// let users = [ vasya, petya, masha ];
+
+// function namify(users) {
+//   // ваш код
+//         let usersNames = users.map((user) => {
+//             return user.name 
+//         });
+//     console.log(usersNames);
+
+// }
+
+// let names = namify(users); // ['Вася', 'Петя', 'Маша']
