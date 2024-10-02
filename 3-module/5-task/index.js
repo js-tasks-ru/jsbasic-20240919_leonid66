65e9208c277f51cc0bade8c10400b85629f9f002
Leonid <sapiroleonid34@gmail.com>
2024-10-03 01:01:07 +0300

    3-module/5-task npm test

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,25 @@
+// function getMinMax(str) {
+//   // ваш код...
+// }
+
+// let inputData = '1 и -5.8 или 10 -5.6 хотя 34 + -5.3 и 73';
+
 function getMinMax(str) {
-  // ваш код...
-}
+  // ...
+      let strToArr = str.split(' ');
+      console.log(strToArr);
+      let numbersOfArr = strToArr.filter( (item) => ( isFinite(item) ) );
+      console.log(numbersOfArr)
+
+      let poryadok = numbersOfArr.sort(function(a, b) {return a - b})
+      console.log(poryadok)
+
+      let result = {};
+      result.min = +poryadok[0];
+      result.max = +poryadok.at(-1);
+
+return result;
+
+};
+
+// console.log(getMinMax(inputData))
