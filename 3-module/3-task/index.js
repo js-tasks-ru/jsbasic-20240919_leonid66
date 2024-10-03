8a17c3af873706ba0d694c5e0e94330a76103081
Leonid <sapiroleonid34@gmail.com>
2024-10-03 10:05:15 +0300

    3-module/3-task fail

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,33 +1,14 @@
-// function camelize(str) {
-//   // ваш код...
+function camelize(str) {
 
+  let words = str.split('-');
 
+  let zagl = words.map( (word, index) => {
+    if (index == 0) {
+      return word
+    } else {
+      return word[0].toUpperCase() + word.slice(1)
+    };
+    } );
 
-  // let str = prompt('vvedi str snake-case','abc-def');
-
-  function camelize(str) {
-  
-          let words = str.split('-');
-    
-    //       for (let word of words ) {
-    //            return word[0].toUpperCase() + word.slice(1);
-    //           };
-
-    //       let result = words.join('');
-    
-    // let result = words.map( (item) = {item[0].toUpperCase() + item.slice(1) } 
-
-    let zagl = words.map( function letter (word) {
-      return word[0].toUpperCase() + word.slice(1) 
-      } );
-
-let result = zagl.join('')
-
-    // console.log(result);
-
-  };
-  
-  // console.log(camelize(str));
-  // camelize(str);
-
-// }
+return zagl.join('');  
+}
