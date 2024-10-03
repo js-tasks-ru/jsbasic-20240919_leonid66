80056c360dfc2d6cee1c33c81868f9b22a307404
Leonid <sapiroleonid34@gmail.com>
2024-10-03 03:14:39 +0300

    3-module/4-task npm test ok

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,12 @@
-function showSalary(users, age) {
-  // ваш код...
-}
+function showSalary(arr, age) {
+    let aged = arr.filter( (user) => ( user.age <= age ) );
+
+    let nameBalArr = aged.map( (user) => {
+       return (user.name + ', ' + user.balance) }
+    )
+  let nameAgeStr = nameBalArr.join('\n');
+  return nameAgeStr;
+
+};
+
+let result = showSalary(users, age);
