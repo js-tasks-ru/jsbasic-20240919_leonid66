e5b513bfad0ef97dc01edebd0ce1d7468702f332
Leonid <sapiroleonid34@gmail.com>
2024-10-11 01:29:13 +0300

    4-module/2-task/index.js

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,7 +1,5 @@
 function makeDiagonalRed(table) {
   // ваш код...
-  let table = document.body.firstElementChild;
-
     for (let i = 0; i < table.rows.length; i++) {
       let row = table.rows[i];
       row.cells[i].style.backgroundColor = 'red';
