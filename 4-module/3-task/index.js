a92b5af3e95a2cb6a59051b61bd94302a9e8be9a
Leonid <sapiroleonid34@gmail.com>
2024-10-11 23:49:18 +0300

    dz 4. N3 doesnt solve

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,38 +1,62 @@
 function highlight(table) {
-  // ваш код...
-for (let tr of table) {
-if (проверить есть ли у строки тр ячейка с тд атрибутом дата-авэйлабл
-  если нет то присвоить хидден) {
-tr.hidden = true;
-} else if (let td = td.querySelector("td[data-available='true']")  /* data-available=="true" */ ){
-  tr.classList.add('available');
-} else if (data-available="false") {
-  tr.classList.add('unavailable');
-}
 
-2 если тд с атрибутом дата-авэйлабл тру - то присвоить класс строке -авэйлабл
-если фолс - то унавеэл
+    let trList = table.querySelectorAll('tr');
+    // console.log(trList);
+    // console.log(trList.length);
 
-// tr.getElementsByTagName("td data-available")
-// document.getElementsByTagName("table")[0].classList.add("yourclass")
-// метод Element.getElementsByTagName() вам может помочь, 
-// a если у вас несколько таблиц, 
-// то просто добавить data-* атрибут и по нему производить поиск.
+  for (let i=2 ; i <= trList.length ; i++) {
+    // console.log(i)
+  //  if (td.classList.contains("td[data-available]") == null) {
+// tr.hidden = true;
+  //  }
 
-// document.querySelector("td[data-available='true']")
+  }
 
-  // td.classList.contains('red');
+  let elementsTrue = document.querySelectorAll('[data-available="true"]');
+      // console.log(elementsTrue);
+    for (td of elementsTrue) {
+      let parentTr = td.parentElement
+        // console.log(parentTr);
+      parentTr.classList.add('available');
+    }
 
-}
-
-}
-
-
-
+  let elementsFalse = document.querySelectorAll('[data-available="false"]');
+      // console.log(elementsFalse);
+    for (td of elementsFalse) {
+      let parentTr = td.parentElement
+        // console.log(parentTr);
+      parentTr.classList.add('unavailable');
+    }
 
 
+  }
 
-// for (let li of document.querySelectorAll('li')) {
-//   ...
+
+
+//   // ваш код...
+// for (let tr of table) {
+// if (проверить есть ли у строки тр ячейка с тд атрибутом дата-авэйлабл
+//   если нет то присвоить хидден) {
+// tr.hidden = true;
+// } else if (let td = td.querySelector("td[data-available='true']")  /* data-available=="true" */ ){
+//   tr.classList.add('available');
+// } else if (data-available="false") {
+//   tr.classList.add('unavailable');
 // }
+
+// 2 если тд с атрибутом дата-авэйлабл тру - то присвоить класс строке -авэйлабл
+// если фолс - то унавеэл
+
+// // tr.getElementsByTagName("td data-available")
+// // document.getElementsByTagName("table")[0].classList.add("yourclass")
+// // метод Element.getElementsByTagName() вам может помочь, 
+// // a если у вас несколько таблиц, 
+// // то просто добавить data-* атрибут и по нему производить поиск.
+
+// // document.querySelector("td[data-available='true']")
+
+//   // td.classList.contains('red');
+
+// }
+
 
