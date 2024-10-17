a36346b53759a1923bf84a891ccc0f5f5a2d78ed
Leonid <sapiroleonid34@gmail.com>
2024-10-17 13:54:07 +0300

    5-3 prohodit test, 4-3 ne prohodit test, no delaet 4to nujno

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,35 @@
 function initCarousel() {
   // ваш код...
+    let arrowR = document.querySelector('.carousel__arrow_right');
+    let arrowL = document.querySelector('.carousel__arrow_left');
+    let slides = document.querySelectorAll('.carousel__slide');
+    let slidesInner = document.querySelector('.carousel__inner');
+    let slideWidth = slidesInner.offsetWidth;
+    let position = 0;
+    arrowL.style.display = 'none';
 
+  arrowR.addEventListener('click', () => {
+      if ( position > slides.length-2 ) { position = slides.length-2 };
+      if ( position === slides.length-2 ) {
+        arrowR.style.display = 'none';
+        arrowL.style.display = '';
+      } else {
+        arrowR.style.display = '';
+        arrowL.style.display = '';
+      };
+      slidesInner.style.transform = `translateX(${ - ( slideWidth * ++position ) + 'px'})`;
+  });
+  
+  arrowL.addEventListener('click', () => {
+      if ( position < 1 ) { position = 1 };
+      if ( position === 1 ) {
+        arrowL.style.display = 'none';
+        arrowR.style.display = '';
+      } else {
+        arrowR.style.display = '';
+        arrowL.style.display = '';
+      };
+      slidesInner.style.transform = `translateX(${ - ( slideWidth * --position ) + 'px'})`;
+  });
+
+};
