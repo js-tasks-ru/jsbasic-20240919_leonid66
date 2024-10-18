779bde16b10f9535649615d4713e3ad676810591
Leonid <sapiroleonid34@gmail.com>
2024-10-19 02:13:11 +0300

    6-1

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,19 +1,53 @@
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      }
- *
- */
 export default class UserTable {
-  constructor(rows) {
-    
-  }
-}
+      constructor(rows) {
+        this.rows = rows; 
+          // console.log(rows)
+        this.render();
+      };       /*    end of constructor    */
+
+      render() {      /*   метод отрисовыки компонента */
+        this.elem = document.createElement('table');    /* insert table */
+
+          this.elem.insertAdjacentHTML('afterBegin',     /* insert thead */
+              ` <thead>
+                  <tr>
+                    <th>Имя</th>
+                    <th>Возраст</th>
+                    <th>Зарплата</th>
+                    <th>Город</th>
+                    <th></th>
+                  </tr>
+                </thead> `
+          );
+
+          this.elem.insertAdjacentHTML(      /*   insert tbody */
+            'beforeEnd',
+            this.rows.map( ( { name, age, salary, city } ) => 
+              ` <tbody>
+                      <tr>
+                          <td> ${name} </td>
+                          <td> ${age} </td>
+                          <td> ${salary} </td>
+                          <td> ${city} </td>
+                          <td id = 'btnX' > <button> X </button> </td>
+                      </tr>
+                  </tbody> `
+              )
+          );
+
+        this.elem.addEventListener('click', this.onClick);
+        // return this.elem;   /*  ?????????? нужен ли здесь? */
+      };      /*   END OF render()    */
+
+      onClick(event) {         /* метод для удаления строки */
+        
+        // let tr = event.target.closest('tr')
+        // tr.remove()     /*   глушит строку при нажатии в любом месте */
+
+        let tdBtnX = event.target.closest('button');
+          // console.log(tdBtnX);
+        let trTdBtnX = tdBtnX.closest('tr');
+          // console.log('tr with btn: ', trTdBtnX);
+          trTdBtnX.remove();
+      };
+};
